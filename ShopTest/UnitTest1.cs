ccc810577d3a06a9882100d82be4fd87ad2beb80
Rogozhinskii <rogozhinskii96@mail.ru>
2021-12-21 00:33:47 +0500

    Подключил Access

--- ShopTest/UnitTest1.cs
+++ ShopTest/UnitTest1.cs
@@ -1,16 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShopLibrary.Authentication;
+using ShopLibrary.DAL;
 using ShopLibrary.Models;
 using ShopLibrary.Services;
+using System.Data;
 using System.Data.Common;
+using System.Data.OleDb;
 using System.Data.SqlClient;
+
 
 namespace ShopTest
 {
     [TestClass]
     public class UnitTest1
     {
-       [TestMethod]
+        [TestMethod]
         public void RegisterTest()
         {
             SqlConnectionStringBuilder connectionString = new()
@@ -19,12 +23,12 @@ namespace ShopTest
                 InitialCatalog = "ShopDb",
                 IntegratedSecurity = true,
                 Pooling = true
-            };            
+            };
             string providerName = "System.Data.SqlClient";
             DbProviderFactories.RegisterFactory(providerName, SqlClientFactory.Instance);
-            UsersService usersService = new UsersService(providerName,connectionString.ToString());
+            UsersService usersService = new UsersService(providerName, connectionString.ToString());
             Protector protector = new(usersService);
-            var result=protector.Register("Admin", "Admin");
+            var result = protector.Register("Admin", "Admin");
             Assert.IsTrue(result);
         }
 
@@ -44,6 +48,41 @@ namespace ShopTest
             Protector protector = new(usersService);
             var result = protector.LogIn("Admin", "Admin");
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AccessConnectionTest()
+        {
+            string providerName = "Microsoft.ACE.OLEDB.12.0";
+            string path = @$"..\..\..\..\ShopLibrary\Access\ShopDb.accdb";
+            DbProviderFactories.RegisterFactory(providerName, OleDbFactory.Instance);            
+            var factory=DbProviderFactories.GetFactory(providerName);
+            OleDbConnectionStringBuilder connectionStringBuilder = new()
+            {
+                Provider = providerName,
+                DataSource = path
+            };
+          
+            IDbConnection connection=factory.CreateConnection();
+            connection.ConnectionString = connectionStringBuilder.ConnectionString;
+            connection.Open();
+
+            string sql = "Insert into Products (email,productId,productName)" +
+                                            "values('rr@mail.ru','32','MSI мышь компьютерная')";
+            IDbCommand cmd=factory.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = connection;
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        
+        
+        }
+
+        [TestMethod]
+        public void ProductsTest()
+        {
+            ProductsDAL productsDAL = new();
         }
     }
 }
